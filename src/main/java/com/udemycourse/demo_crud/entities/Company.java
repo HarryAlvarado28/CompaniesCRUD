cc4941b3a316541ed58de4ad8392a5a8d7d9e818
Harry Alvarado <harry2alvarado8@hotmail.com>
2024-03-13 08:20:34 -0500

    Definiendo capa de servicio

--- src/main/java/com/udemycourse/demo_crud/entities/Company.java
+++ src/main/java/com/udemycourse/demo_crud/entities/Company.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table
@@ -22,4 +23,8 @@ public class Company {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate foundationDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "id_company", referencedColumnName = "id")
+    private List<WebSite> webSiteList;
 }
