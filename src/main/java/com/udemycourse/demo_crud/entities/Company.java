6862c0a357cc459bc5d0b6cad0126396d5fff9cd
Harry Alvarado <harry2alvarado8@hotmail.com>
2024-03-12 19:05:44 -0500

    Mapeando entidades JPA - Companies

--- src/main/java/com/udemycourse/demo_crud/entities/Company.java
+++ src/main/java/com/udemycourse/demo_crud/entities/Company.java
@@ -1,4 +1,25 @@
 package com.udemycourse.demo_crud.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
 public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    private String founder;
+
+    private String logo;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate foundationDate;
 }
