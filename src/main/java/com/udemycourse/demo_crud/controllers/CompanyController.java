6359c9b826fd0e7c7bc78a3a7e3e771cabeac6d5
Harry Alvarado <harry2alvarado8@hotmail.com>
2024-03-13 10:58:51 -0500

    Agregando swagger

--- src/main/java/com/udemycourse/demo_crud/controllers/CompanyController.java
+++ src/main/java/com/udemycourse/demo_crud/controllers/CompanyController.java
@@ -2,6 +2,8 @@ package com.udemycourse.demo_crud.controllers;
 
 import com.udemycourse.demo_crud.entities.Company;
 import com.udemycourse.demo_crud.services.CompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,27 +15,32 @@ import java.net.URI;
 @AllArgsConstructor
 @RequestMapping(path = "company")
 @Slf4j
+@Tag(name= "Companies resource")
 public class CompanyController {
     private final CompanyService companyService;
 
+    @Operation(summary = "get a company given a company name")
     @GetMapping(path = "{name}")
     public ResponseEntity<Company> get(@PathVariable String name){
         log.info("GET: company {}",name);
         return ResponseEntity.ok(this.companyService.readByName(name));
     }
 
+    @Operation(summary = "save in DB a company given a company from body")
     @PostMapping
     public ResponseEntity<Company> post(@RequestBody Company company){
         log.info("POST: company {}",company.getName());
         return ResponseEntity.created(URI.create(this.companyService.create(company).getName())).build();
     }
 
+    @Operation(summary = "update in DB a company given a company from body")
     @PutMapping(path = "{name}")
     public ResponseEntity<Company> put(@RequestBody Company company, @PathVariable String name){
         log.info("PUT: company {}",name);
         return ResponseEntity.ok(this.companyService.update(company, name));
     }
 
+    @Operation(summary = "delete in DB a company given a company for name")
     @DeleteMapping(path = "{name}")
     public ResponseEntity<?> delete(@PathVariable String name){
         log.info("DELETE: company {}",name);
